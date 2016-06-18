09a740a3fe51a01434ae0a67e0e48208fd6014b7
akalenda <akalenda@users.noreply.github.com>
2016-06-17 22:12:45 -0700

    Issue#3: Documented Chandy-Misra

--- samples/ChandyMisra.js
+++ samples/ChandyMisra.js
@@ -1,5 +1,15 @@
 /*
- * @fileoverview Your description here
+ * @fileoverview From Wan Fokkink's "Distributed Algorithms: An Intuitive Approach", the Chandy-Misra algorithm computes
+ * a sink a tree routed in the initiator. That is to say, after the algorithm completes, each process in the network
+ * has a (supposedly) shortest path to the initiator.
+ *
+ * At first this may appear similar to the Echo algorithm. However, Echo will not make alterations to the tree after
+ * parents have first been set. Chandy-Misra is designed for many consecutive executions of the algorithm without a
+ * reset, which may even execute on top of one another. This is most apparent in a densely connected graph. It is also
+ * more robust in handling processes in the networks coming and going on- and off-line.
+ *
+ * Properly, it would be accompanied by a termination detection algorithm such as Djikstra-Scholten's, however, for the
+ * purposes of this application visual confirmation is good enough to understand Chandy-Misra's workings.
  */
 
 //noinspection JSUnresolvedFunction
@@ -16,7 +26,7 @@ onInitializationDo(
      * @param {Process} p
      */
     function (p) {
-        p.dist = "infinity";
+        p.dist = Infinity;
     }
 );
 
@@ -44,7 +54,7 @@ onReceivingMessageDo(
         if (message.dist !== undefined) {
             var d = message.dist;
             var d2 = d + p.getDistanceTo(q);
-            if (p.dist === "infinity" || d2 < p.dist) {
+            if (p.dist === Infinity || d2 < p.dist) {
                 p.dist = d2;
                 p.setParentTo(q);
                 p.sendEachOutgoingChannelExcept(q, {dist: d2});
